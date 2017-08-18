e9ee9ee9a2123f7c939bc2ad3d27b69955aab54d
Tihosov Tolik <Tihosov Tolik>
2017-08-18 23:23:32 +0300

    Some Ntive changes

--- App.js
+++ App.js
@@ -2,10 +2,21 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+        super(props);
+        this.state = {display: 'none'};
+  }
+
   render() {
+      function showText() {
+        this.setState({display: this.state.display === 'none' ? 'block' : 'none'})
+      }
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.button} onClick={() => {this.showText()}}>
+          <Text style={styles.buttonText}>Show</Text>
+        </View>
+          <Text style={styles.text}>{this.state.text}</Text>
       </View>
     );
   }
@@ -18,4 +29,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+    text: {
+        color: 'red',
+        fontSize: 18
+    },
+
+    button: {
+        backgroundColor: 'blue',
+        width: 80,
+        height: 80,
+        borderRadius: 40,
+        border: 40
+    },
+
+    buttonText: {
+        color: 'white',
+        fontSize: 20,
+    }
 });
